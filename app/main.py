a4bfeb66ffd15567cc12f1e5fb02ce331b95f913
lululu957 <lucasbaudingoblet@gmail.com>
2025-10-14 14:03:36 +0200

    runserver

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from app.api import user_routes
+from app.api import user_routes, auth_routes
 
 app = FastAPI(title="Bank API")
 
 app.include_router(user_routes.router)
+
+app = FastAPI()
+
+app.include_router(user_routes.router)
+app.include_router(auth_routes.router)
