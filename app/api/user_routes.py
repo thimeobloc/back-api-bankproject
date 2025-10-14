c1fd062f5c7af26a9802582cf10275fde1ea2c4a
thimeobloc <thimeo.ozaneaux@gmail.com>
2025-10-14 10:32:50 +0200

    add user / create / email / password hashing / story 1

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,1 +1,19 @@
- 
+from fastapi import APIRouter
+from app.schemas.user_schemas import UserCreate
+from app.core.security import hash_password
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+users_db = []
+
+@router.post("/", response_model=UserCreate)
+def create_user_endpoint(user: UserCreate):
+    hashed_password = hash_password(user.password)
+    user_dict = user.dict()
+    user_dict["password"] = hashed_password
+    users_db.append(user_dict)
+    return user_dict
+
+@router.get("/", response_model=list[UserCreate])
+def list_users_endpoint():
+    return users_db
--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from app.api import user_routes
 
-app = FastAPI()
+app = FastAPI(title="Bank API")
 
-@app.get("/")
-def root():
-    return {"message": "Hello NovaBank!"}
-    
+app.include_router(user_routes.router)
